c0427512aae3691a6590f83517203d0a0ec92bce
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-08-08 20:50:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Tracket());
+}
+
+class Tracket extends StatelessWidget {
+  const Tracket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
