cbabb31f1fba10c36b54811a1abb2023f51976f4
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-11-22 00:07:46 +0530

    create firebase auth method

---
+++ lib/resources/firebase_auth_methods.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthMethods {
+  static final _auth = FirebaseAuth.instance;
+
+  Future<void> signupUser({
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    
+  }
+}
