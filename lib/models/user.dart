d14f70a6ee0a845dfc0ccfd9566406642430cee1
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-11-23 19:16:32 +0530

    wrote a method for signup user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,14 +2,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   User({
+    required this.userId,
     required this.email,
     required this.username,
     required this.role,
     required this.createdAt,
   });
 
+  final String userId;
   final String email;
   final String username;
   final String role;
   final Timestamp createdAt;
+
+  Map<String, dynamic> get toJson => {
+        'userId': userId,
+        'email': email,
+        'username': username,
+        'role': role,
+        'createdAt': createdAt,
+      };
 }
--- lib/resources/firebase_auth_methods.dart
+++ lib/resources/firebase_auth_methods.dart
@@ -1,13 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tracket/models/user.dart' as model;
+import 'package:uuid/uuid.dart';
 
 class FirebaseAuthMethods {
   static final _auth = FirebaseAuth.instance;
+  static final _firestore = FirebaseFirestore.instance;
+  static const uuid = Uuid();
 
-  Future<void> signupUser({
+  Future<String> signupUser({
     required String username,
     required String email,
     required String password,
   }) async {
-    
+    String result;
+
+    try {
+      final userCred = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final model.User user = model.User(
+        email: email,
+        username: username,
+        role: 'user',
+        createdAt: Timestamp.now(),
+        userId: userCred.user!.uid,
+      );
+
+      await _firestore
+          .collection('users')
+          .doc(userCred.user!.uid)
+          .set(user.toJson);
+      result = 'success';
+    } catch (e) {
+      result = e.toString();
+    }
+
+    return result;
   }
 }
