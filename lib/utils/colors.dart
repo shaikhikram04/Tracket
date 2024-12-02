cba23482827d61d04911414ca36e30de58de3e6d
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-02 23:14:43 +0530

    added a default profile picture & dragger structure

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 const lightBackgroundColor = Color.fromARGB(255, 239, 250, 239);
 const greenColor = Colors.green;
 const lightCardColor = Color.fromARGB(255, 201, 234, 205);
+const lightDrawerBgColor = Color.fromARGB(255, 83, 158, 86);
+const onLightDrawer = Color.fromARGB(255, 221, 229, 217);
 
 //* Darkmode colors
 const darkBackgroundColor = Color.fromRGBO(33, 33, 33, 1);
