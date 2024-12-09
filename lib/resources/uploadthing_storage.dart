f11ec313eca313654c3fb16e2d518d2cbec5d78f
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-09 20:44:43 +0530

    added imagePicker function

--- lib/resources/uploadthing_storage.dart
+++ lib/resources/uploadthing_storage.dart
@@ -1,3 +1,1 @@
-class UploadthingStorage {
-  
-}
+class UploadthingStorage {}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tracket/screens/authentication/verification_screen.dart';
 import 'package:tracket/utils/colors.dart';
 
@@ -36,4 +39,15 @@ void showAlertDialog(BuildContext context, String title, String errorMessage) {
       );
     },
   );
+}
+
+Future<Uint8List?> pickImage(ImageSource source) async {
+  final ImagePicker imagePicker = ImagePicker();
+
+  XFile? file = await imagePicker.pickImage(source: source);
+
+  if (file != null) {
+    return await file.readAsBytes();
+  }
+  return null;
 }
