ae2a2bdbdf0fbd79dc59872b929c79be5635a1c4
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-15 19:13:18 +0530

    showing squad

--- lib/screens/team_details_screen.dart
+++ lib/screens/team_details_screen.dart
@@ -28,103 +28,149 @@ class TeamDetailsScreen extends StatelessWidget {
         centerTitle: false,
         shape: Border.all(color: greenColor, width: 0),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //! Team Logo & name
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  greenColor,
-                  lightDrawerBgColor,
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //! Team Logo & name
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    greenColor,
+                    lightDrawerBgColor,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
               ),
-            ),
-            height: height * 0.23,
-            width: width,
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('assets/images/team_logo.png'),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Team Name',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                Text(
-                  'TSN',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          //! Team Stats
-          Card(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            color: whiteColor,
-            elevation: 7,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+              height: height * 0.23,
+              width: width,
               child: Column(
                 children: [
-                  Text(
-                    'Team Statistics',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium!
-                        .copyWith(fontSize: 23),
+                  const SizedBox(height: 10),
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/images/team_logo.png'),
                   ),
-                  const SizedBox(height: 17),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      StatsData(
-                        number: 256,
-                        label: 'Matches',
-                        numColor: Color.fromARGB(255, 29, 130, 212),
-                      ),
-                      StatsData(
-                        number: 148,
-                        label: 'Wins',
-                        numColor: Color.fromARGB(255, 39, 141, 42),
-                      ),
-                      StatsData(
-                        number: 102,
-                        label: 'Losses',
-                        numColor: Colors.red,
-                      ),
-                      StatsData(
-                        number: 6,
-                        label: 'Ties',
-                        numColor: Colors.amber,
-                      ),
-                    ],
+                  const SizedBox(height: 10),
+                  Text(
+                    'Team Name',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Text(
+                    'TSN',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          //! Players Detail
-          const Card(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            color: whiteColor,
-            elevation: 7,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-              child: Column(
-                children: [Text('Squrd')],
+            const SizedBox(height: 20),
+            //! Team Stats
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              color: whiteColor,
+              elevation: 7,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+                child: Column(
+                  children: [
+                    Text(
+                      'Team Statistics',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(fontSize: 23),
+                    ),
+                    const SizedBox(height: 17),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        StatsData(
+                          number: 256,
+                          label: 'Matches',
+                          numColor: Color.fromARGB(255, 29, 130, 212),
+                        ),
+                        StatsData(
+                          number: 148,
+                          label: 'Wins',
+                          numColor: Color.fromARGB(255, 39, 141, 42),
+                        ),
+                        StatsData(
+                          number: 102,
+                          label: 'Losses',
+                          numColor: Colors.red,
+                        ),
+                        StatsData(
+                          number: 6,
+                          label: 'Ties',
+                          numColor: Colors.amber,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            //! Players Detail
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              color: whiteColor,
+              elevation: 7,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Squad',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(fontSize: 23),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      children: List.generate(
+                        6,
+                        (index) {
+                          return ListTile(
+                            onTap: () {},
+                            leading: const CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                'assets/images/Default_user_pfp.jpg',
+                              ),
+                            ),
+                            title: Row(
+                              children: [
+                                Text(
+                                  'Player Name',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .copyWith(fontSize: 18),
+                                ),
+                                const SizedBox(width: 7),
+                                
+                              ],
+                            ),
+                            subtitle: const Text('Cricket Role'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 40),
+          ],
+        ),
       ),
     );
   }
