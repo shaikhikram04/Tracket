2ad3276213aa54fc1e596c7dd556a3fe2a148e6d
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-25 15:23:49 +0530

    using MyCard

--- lib/screens/teams/team_profile_screen.dart
+++ lib/screens/teams/team_profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:tracket/models/team.dart';
 import 'package:tracket/screens/teams/add_player_screen.dart';
 import 'package:tracket/screens/teams/team_edit_screen.dart';
 import 'package:tracket/utils/colors.dart';
+import 'package:tracket/widgets/my_card.dart';
 import 'package:tracket/widgets/player_tile.dart';
 import 'package:tracket/widgets/stats_data.dart';
 
@@ -87,145 +88,133 @@ class TeamProfileScreen extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             //! Team Stats
-            Card(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              color: whiteColor,
-              elevation: 7,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-                child: Column(
-                  children: [
-                    Text(
-                      'Team Statistics',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .copyWith(fontSize: 23),
-                    ),
-                    const SizedBox(height: 17),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        StatsData(
-                          number: teamData.matchesPlayed,
-                          label: 'Matches',
-                          numColor: const Color.fromARGB(255, 29, 130, 212),
-                        ),
-                        StatsData(
-                          number: teamData.wins,
-                          label: 'Wins',
-                          numColor: const Color.fromARGB(255, 39, 141, 42),
-                        ),
-                        StatsData(
-                          number: teamData.losses,
-                          label: 'Losses',
-                          numColor: Colors.red,
-                        ),
-                        StatsData(
-                          number: teamData.tieCount,
-                          label: 'Ties',
-                          numColor: Colors.amber,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            MyCard(
+              child: Column(
+                children: [
+                  Text(
+                    'Team Statistics',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(fontSize: 23),
+                  ),
+                  const SizedBox(height: 17),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      StatsData(
+                        number: teamData.matchesPlayed,
+                        label: 'Matches',
+                        numColor: const Color.fromARGB(255, 29, 130, 212),
+                      ),
+                      StatsData(
+                        number: teamData.wins,
+                        label: 'Wins',
+                        numColor: const Color.fromARGB(255, 39, 141, 42),
+                      ),
+                      StatsData(
+                        number: teamData.losses,
+                        label: 'Losses',
+                        numColor: Colors.red,
+                      ),
+                      StatsData(
+                        number: teamData.tieCount,
+                        label: 'Ties',
+                        numColor: Colors.amber,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
+
             const SizedBox(height: 20),
             //! Players Detail
-            Card(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              color: whiteColor,
-              elevation: 7,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 25),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Squad',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .copyWith(fontSize: 23),
-                          ),
-                          const Spacer(),
-                          IconButton(
-                            onPressed: addPlayer,
-                            iconSize: 30,
-                            icon: const Icon(Icons.group_add),
-                          )
-                        ],
-                      ),
+            MyCard(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Squad',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(fontSize: 23),
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          onPressed: addPlayer,
+                          iconSize: 30,
+                          icon: const Icon(Icons.group_add),
+                        )
+                      ],
                     ),
-                    const SizedBox(height: 10),
-                    teamData.playersList.isEmpty
-                        ? Center(
-                            child: Column(
-                              children: [
-                                Icon(
-                                  Icons.group_off,
-                                  size: 80,
-                                  color: Colors.grey.shade400,
+                  ),
+                  const SizedBox(height: 10),
+                  teamData.playersList.isEmpty
+                      ? Center(
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.group_off,
+                                size: 80,
+                                color: Colors.grey.shade400,
+                              ),
+                              const SizedBox(height: 20),
+                              Text(
+                                "No Player joined yet!",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey.shade600,
                                 ),
-                                const SizedBox(height: 20),
-                                Text(
-                                  "No Player joined yet!",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.grey.shade600,
-                                  ),
-                                  textAlign: TextAlign.center,
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 20),
+                              Text(
+                                "Tap the button above to request player to join.",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey.shade500,
                                 ),
-                                const SizedBox(height: 20),
-                                Text(
-                                  "Tap the button above to request player to join.",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey.shade500,
-                                  ),
-                                  textAlign: TextAlign.center,
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 40),
+                              ZoomIn(
+                                child: Icon(
+                                  Icons.arrow_outward,
+                                  size: 50,
+                                  color: Colors.green.shade400,
                                 ),
-                                const SizedBox(height: 40),
-                                ZoomIn(
-                                  child: Icon(
-                                    Icons.arrow_outward,
-                                    size: 50,
-                                    color: Colors.green.shade400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                        : Column(
-                            children: List.generate(
-                            teamData.playersList.length,
-                            (index) {
-                              final playerDetail = teamData.playersList[index];
-                              final playerId = playerDetail['id'];
-                              final isCaptain = teamData.captainId == null
-                                  ? false
-                                  : teamData.captainId == playerId;
-                              final isWicketKeeper =
-                                  teamData.wicketKeeperId == null
-                                      ? false
-                                      : teamData.wicketKeeperId == playerId;
-                              return PlayerTile(
-                                isCaptain: isCaptain,
-                                isWicketKeeper: isWicketKeeper,
-                                playerName: playerDetail['name'],
-                                cricketRole: playerDetail['cricketRole'],
-                              );
-                            },
-                          )),
-                  ],
-                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Column(
+                          children: List.generate(
+                          teamData.playersList.length,
+                          (index) {
+                            final playerDetail = teamData.playersList[index];
+                            final playerId = playerDetail['id'];
+                            final isCaptain = teamData.captainId == null
+                                ? false
+                                : teamData.captainId == playerId;
+                            final isWicketKeeper =
+                                teamData.wicketKeeperId == null
+                                    ? false
+                                    : teamData.wicketKeeperId == playerId;
+                            return PlayerTile(
+                              isCaptain: isCaptain,
+                              isWicketKeeper: isWicketKeeper,
+                              playerName: playerDetail['name'],
+                              cricketRole: playerDetail['cricketRole'],
+                            );
+                          },
+                        )),
+                ],
               ),
             ),
 
