1f674c43e6f2c41ef9b0581f4029cb763234ffc7
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-26 16:02:12 +0530

    manage file structure for authentication

---
+++ lib/authentication/screens/auth_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tracket/provider/auth_screen_size.dart';
+import 'package:tracket/authentication/player_auth.dart';
+import 'package:tracket/authentication/user_auth.dart';
+
+class AuthScreen extends ConsumerStatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  ConsumerState<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends ConsumerState<AuthScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final tabBarViewHeight = ref.watch(authScreenSizeProvider);
+    // Height of the status bar
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+    // Height of the bottom inset (e.g., navigation bar)
+    final double bottomInsetHeight = MediaQuery.of(context).padding.bottom;
+    final safeAreaHeight = height - statusBarHeight - bottomInsetHeight;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: safeAreaHeight),
+            child: Column(
+              children: [
+                const SizedBox(height: 15),
+                //* App logo
+                Image.asset(
+                  'assets/images/Tracket_logo.png',
+                  height: height * 0.25,
+                  fit: BoxFit.cover,
+                ),
+                //* Auth content
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 17,
+                    vertical: 12,
+                  ),
+                  child: Card(
+                    color: Theme.of(context).cardColor,
+                    child: Column(
+                      children: [
+                        //* TabBar for show option of user & player authentication
+                        TabBar(
+                          dividerColor: Theme.of(context).colorScheme.secondary,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          controller: _tabController,
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          unselectedLabelStyle:
+                              const TextStyle(fontWeight: FontWeight.normal),
+                          tabs: const [
+                            Tab(
+                              child: Text(
+                                'Player',
+                                style: TextStyle(fontSize: 21),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                'User',
+                                style: TextStyle(fontSize: 21),
+                              ),
+                            ),
+                          ],
+                          onTap: (value) async {
+                            if (_tabController.indexIsChanging) {
+                              await Future.delayed(
+                                  const Duration(milliseconds: 180));
+                              ref
+                                  .read(authScreenSizeProvider.notifier)
+                                  .changeScreen(
+                                    value == 0
+                                        ? AuthScreenType.playerLogin
+                                        : AuthScreenType.userLogin,
+                                  );
+                            }
+                          },
+                        ),
+
+                        //* Content of TabBar for signup/login user or player
+                        SizedBox(
+                          height: tabBarViewHeight,
+                          child: TabBarView(
+                            physics: const NeverScrollableScrollPhysics(),
+                            controller: _tabController,
+                            children: const [
+                              PlayerAuth(),
+                              UserAuth(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/authentication/screens/forget_password.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:tracket/resources/firebase_auth_methods.dart';
+import 'package:tracket/utils/colors.dart';
+import 'package:tracket/utils/utils.dart';
+import 'package:tracket/widgets/custom_widgets/my_text_button.dart';
+import 'package:tracket/widgets/custom_widgets/my_text_field.dart';
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({
+    super.key,
+  });
+
+  @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  String? _email;
+  late GlobalKey<FormState> _formKey;
+  late bool _isResetEmailSend;
+  late bool _isSendingEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _isResetEmailSend = false;
+    _isSendingEmail = false;
+  }
+
+  Future<void> _sendResetEmail() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      setState(() {
+        _isSendingEmail = true;
+      });
+      final result = await FirebaseAuthMethods.resetPassword(_email!);
+      setState(() {
+        _isSendingEmail = false;
+      });
+      if (result == 'success') {
+        setState(() {
+          _isResetEmailSend = true;
+        });
+      } else {
+        if (!mounted) return;
+        showSnackBar('Some error occur. Please try again!', context);
+      }
+    }
+  }
+
+  void _onEditEmail() {
+    setState(() {
+      _isResetEmailSend = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _formKey.currentState?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final message = _isResetEmailSend
+        ? 'Reset email link has been send to ${_email!.trim()}. Check your inbox if you registered!'
+        : 'Enter the register email. We will send the reset email on provided email.';
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Image.asset(
+                  'assets/images/Tracket_logo.png',
+                  height: height * 0.25,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  'Forget Password?',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Card(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  color: Theme.of(context).cardColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          message,
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(color: Colors.black),
+                        ),
+                        const SizedBox(height: 20),
+                        if (!_isResetEmailSend)
+                          MyTextField(
+                            isLogin: true,
+                            onSave: (value) => _email = value,
+                            label: 'Email',
+                            borderRadius: 15,
+                          ),
+                        const SizedBox(height: 15),
+                        Row(
+                          children: [
+                            if (_isResetEmailSend)
+                              MyTextButton(
+                                text: 'Edit email',
+                                onPressed: _onEditEmail,
+                              ),
+                            const Spacer(),
+                            if (_isSendingEmail)
+                              const SizedBox.square(
+                                dimension: 20,
+                                child: CircularProgressIndicator(),
+                              ),
+                            MyTextButton(
+                              text: _isResetEmailSend
+                                  ? 'Resend'
+                                  : 'Send reset email',
+                              onPressed: _sendResetEmail,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: blackColor,
+                  ),
+                  label: Text(
+                    'Back to login',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(fontWeight: FontWeight.w500),
+                  ),
+                  icon: const Icon(Icons.arrow_back),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
