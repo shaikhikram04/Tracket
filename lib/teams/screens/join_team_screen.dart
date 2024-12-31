3f33d3327e6e7c5e5bad63ac4c632e13cbf37599
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-12-31 23:10:12 +0530

    write a method for join Team

--- lib/teams/screens/join_team_screen.dart
+++ lib/teams/screens/join_team_screen.dart
@@ -1,11 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tracket/players/models/player.dart';
 import 'package:tracket/players/providers/player_provider.dart';
 import 'package:tracket/resources/firestore_collections.dart';
+import 'package:tracket/resources/firestore_methods.dart';
 import 'package:tracket/teams/models/team.dart';
 import 'package:tracket/teams/providers/request_status_provider.dart';
-import 'package:tracket/teams/providers/team_provider.dart';
 import 'package:tracket/teams/screens/team_profile_screen.dart';
 import 'package:tracket/utils/colors.dart';
 import 'package:tracket/utils/utils.dart';
@@ -19,15 +21,16 @@ class JoinTeamScreen extends ConsumerStatefulWidget {
 
 class _JoinTeamScreenState extends ConsumerState<JoinTeamScreen> {
   late final List<String> playerTeamsId;
+  late Player _player;
 
   @override
   void initState() {
     super.initState();
-    playerTeamsId =
-        ref.read(playerProvider).teams!.map((e) => e['id'].toString()).toList();
+    _player = ref.read(playerProvider);
+    playerTeamsId = _player.teams!.map((e) => e['id'].toString()).toList();
   }
 
-  void toggleAdding(String playerId, bool isAdding) {
+  void toggleJoining(String playerId, bool isAdding) {
     if (isAdding) {
       ref.read(requestStatusProvider.notifier).addRequestInProgress(playerId);
     } else {
@@ -37,7 +40,39 @@ class _JoinTeamScreenState extends ConsumerState<JoinTeamScreen> {
 
   void joinTeam(
     String teamId,
-  ) async {}
+    String teamName,
+    String teamShortName,
+    String? teamLogoUrl,
+  ) async {
+    Map<String, dynamic> teamInfo = {
+      'id': teamId,
+      'name': teamName,
+      'shortName': teamShortName,
+      'logoUrl': teamLogoUrl,
+      'role': 'player',
+    };
+    Map<String, dynamic> playerInfo = {
+      'id': _player.id,
+      'name': _player.name,
+      'cricketRole': _player.cricketRole!.name,
+      'imageUrl': _player.profileImageUrl,
+      'role': 'player',
+    };
+    toggleJoining(teamId, true);
+    try {
+      FirestoreMethods.addPlayerToTeam(
+          playerInfo: playerInfo,
+          teamInfo: teamInfo,
+          ref: ref,
+          context: context);
+    } catch (e) {
+      if (context.mounted) {
+        showSnackBar('Failed to join team. Please try again later.', context);
+      }
+    } finally {
+      toggleJoining(teamId, false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,38 +122,73 @@ class _JoinTeamScreenState extends ConsumerState<JoinTeamScreen> {
             itemCount: snapshot.data!.size,
             itemBuilder: (context, index) {
               final teamData = Team.formSeed(snap[index].data(), null);
-              final isJoined = playerTeamsId.contains(teamData.id);
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: teamData.logoUrl == null
-                      ? const AssetImage('assets/images/team_logo.png')
-                      : NetworkImage(teamData.logoUrl!),
-                  radius: 30,
-                ),
-                title: Text(teamData.name),
-                subtitle: Text(teamData.shortName),
-                onTap: () {
-                  ref.read(teamProvider.notifier).updateTeam(teamData);
-                  pushScreen(context, const TeamProfileScreen());
-                },
-                trailing: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: isJoined ? Colors.grey : buttonBgColor,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                  ),
-                  onPressed: isJoined ? null : () {},
-                  child: Text(
-                    isJoined ? 'Joined' : 'Join',
-                    style: const TextStyle(
-                      color: blackColor,
-                    ),
-                  ),
-                ),
-              );
+              return buildTeamTile(teamData);
             },
           );
         },
+      ),
+    );
+  }
+
+  Widget buildTeamTile(Team team) {
+    final bool isTeamPrivate = team.isPrivate;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: team.logoUrl != null
+              ? CachedNetworkImageProvider(team.logoUrl!)
+              : const AssetImage('assets/images/team_logo.png'),
+          radius: 30,
+        ),
+        title: Text(team.name),
+        subtitle: Text(team.shortName),
+        onTap: () => pushScreen(context, const TeamProfileScreen()),
+        trailing: Consumer(
+          builder: (context, ref, _) {
+            final requestStatus = ref.watch(requestStatusProvider);
+            final isRequestInProgress =
+                requestStatus.requestInProgress.contains(team.id);
+            final isRequestSuccess =
+                requestStatus.requestSuccess.contains(team.id);
+
+            final isJoined =
+                playerTeamsId.contains(team.id) || isRequestSuccess;
+            final buttonText = isJoined
+                ? 'Joined'
+                : isTeamPrivate
+                    ? 'Request'
+                    : 'Join';
+            final isButtonDisabled = isJoined || isRequestInProgress;
+
+            return ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isButtonDisabled ? Colors.grey : buttonBgColor,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                fixedSize: const Size(95, 35),
+              ),
+              onPressed: isButtonDisabled
+                  ? null
+                  : () => joinTeam(
+                        team.id,
+                        team.name,
+                        team.shortName,
+                        team.logoUrl,
+                      ),
+              child: isRequestInProgress
+                  ? const SizedBox.square(
+                      dimension: 20,
+                      child: CircularProgressIndicator(strokeWidth: 2.5),
+                    )
+                  : Text(
+                      buttonText,
+                      style: const TextStyle(color: blackColor),
+                    ),
+            );
+          },
+        ),
       ),
     );
   }
