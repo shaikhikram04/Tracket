979d37ae0839346517092a0a344f739dadd484ae
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-04 23:00:57 +0530

    Added a floating Speeddial in matchScreen

--- lib/matches/screens/matches_screen.dart
+++ lib/matches/screens/matches_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:tracket/matches/widgets/match_card.dart';
 
 class MatchesScreen extends StatelessWidget {
@@ -13,6 +14,31 @@ class MatchesScreen extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return const MatchCard();
         },
+      ),
+      floatingActionButton: SpeedDial(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        animatedIcon: AnimatedIcons.menu_close,
+        overlayOpacity: 0.45,
+        overlayColor: Colors.black,
+        animationDuration: const Duration(milliseconds: 280),
+        animationCurve: Curves.easeInOut,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.schedule),
+            label: 'Schedule Match',
+            onTap: () {
+              // pushScreen(context, JoinTeamScreen(player));
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.create),
+            label: 'Create Match',
+            onTap: () {
+              // pushScreen(context, const CreateTeamScreen());
+            },
+          ),
+        ],
       ),
     );
   }
