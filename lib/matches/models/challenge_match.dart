6c4a29f4794f60fabfa03ee683bc1b795859ef9e
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-08 20:28:23 +0530

    created challenge match model

---
+++ lib/matches/models/challenge_match.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+enum ChallengeStatus {
+  pending,
+  accept,
+  decline,
+}
+
+class MatchDetail {
+  const MatchDetail({
+    required this.date,
+    required this.venue,
+    required this.time,
+    required this.overs,
+  });
+  final int overs;
+  final String venue;
+  final DateTime date;
+  final TimeOfDay time;
+}
+
+class ChallengeMatch {
+  ChallengeMatch({
+    required this.challengeStatus,
+    required this.challengedTeam,
+    required this.challengerId,
+    required this.challengerName,
+    required this.challengerTeam,
+    required this.createdAt,
+    required this.matchDetails,
+    required this.matchId,
+    required this.updatedAt,
+    required this.willLive,
+  });
+
+  final String matchId;
+  final ChallengeStatus challengeStatus;
+  final Map<String, dynamic> challengerTeam;
+  final Map<String, dynamic> challengedTeam;
+  final String challengerId;
+  final String challengerName;
+  final MatchDetail matchDetails;
+  final bool willLive;
+  final Timestamp createdAt;
+  final Timestamp updatedAt;
+}
