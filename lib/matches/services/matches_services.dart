c982f5b8440244243adceff7b6e02a7069ce0031
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-13 15:48:23 +0530

    remove not needed model

--- lib/matches/services/matches_services.dart
+++ lib/matches/services/matches_services.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:tracket/notifications/models/challenge_match.dart';
 import 'package:tracket/matches/models/match.dart';
-import 'package:tracket/matches/models/match_detail.dart';
+import 'package:tracket/notifications/models/challenge_match.dart';
 import 'package:tracket/players/models/player_details.dart';
 import 'package:tracket/teams/models/team_details.dart';
 import 'package:tracket/utils/utility_classes/firestore_collections.dart';
@@ -32,42 +31,23 @@ class MatchesServices {
       matchTime.hour,
       matchTime.minute,
     );
-    final matchDetail = MatchDetail(
-      schedule: schedule,
-      venue: matchVenue,
-      overs: MatchFormat.values[matchFormatIndex],
-    );
 
     final challengeMatch = ChallengeMatch(
-      challengeStatus: ChallengeStatus.pending,
       challengedTeam: challengedTeam,
       challengerId: challengerId,
       challengerName: challengerName,
       challengerTeam: challengerTeam,
-      createdAt: Timestamp.now(),
-      matchDetails: matchDetail,
-      matchId: _uuid.v4(),
       updatedAt: Timestamp.now(),
       willLive: allowSpectators,
       challengedPlayers: [],
       challengerPlayers: challengerPlayers,
       noOfPlayers: noOfPlayers,
-      challengedTeamId: challengedTeam.id,
-      challengerTeamId: challengerTeam.id,
+      schedule: schedule,
+      venue: matchVenue,
+      overs: MatchFormat.values[matchFormatIndex],
     );
 
-    await _firestore
-        .collection(FirestoreCollections.challengeMatch)
-        .doc(challengeMatch.matchId)
-        .set(challengeMatch.toMap);
-
-    for (final player in challengerPlayers) {
-      await _firestore
-          .collection(FirestoreCollections.challengeMatch)
-          .doc(challengeMatch.matchId)
-          .collection(FirestoreCollections.challengerPlayers)
-          .doc(player.id)
-          .set(player.toMap);
-    }
+    
+    
   }
 }
