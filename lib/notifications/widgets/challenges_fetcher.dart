6fd3a2ffbc6ed9ee0ac91b557be107e629ad1cf7
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-15 21:24:21 +0530

    created challenges fetcher

---
+++ lib/notifications/widgets/challenges_fetcher.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tracket/players/providers/player_provider.dart';
+import 'package:tracket/utils/utility_classes/firestore_collections.dart';
+import 'package:tracket/widgets/no_data_found.dart';
+
+class ChallengesFetcher extends ConsumerWidget {
+  const ChallengesFetcher({
+    super.key,
+    required this.field,
+  });
+
+  final String field;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ids = [];
+
+    final player = ref.watch(playerProvider);
+    final playerTeamsId = player.playerTeamsId;
+    ids.addAll([...playerTeamsId, player.id]);
+
+    return FutureBuilder(
+      future: FirebaseFirestore.instance
+          .collection(FirestoreCollections.notification)
+          .where(field, whereIn: ids)
+          .where('type', whereIn: ['matchChallenge']).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const NoDataFound(
+            title: 'No request found',
+            message: '',
+            isRequest: true,
+          );
+        }
+
+        final challenges = snapshot.data!.docs;
+        return Container();
+      },
+    );
+  }
+}
