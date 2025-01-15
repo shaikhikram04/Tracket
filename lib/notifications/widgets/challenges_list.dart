fdc4564996a326cc866e0d1bc9c550dfdca209e8
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-15 22:14:43 +0530

    created challenges list

---
+++ lib/notifications/widgets/challenges_list.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tracket/notifications/models/notification.dart' as model;
+import 'package:tracket/notifications/services/notification_services.dart';
+import 'package:tracket/teams/providers/request_status_provider.dart';
+import 'package:tracket/utils/utils.dart';
+
+class ChallengesList extends StatefulWidget {
+  const ChallengesList({
+    super.key,
+    required this.challenge,
+    required this.isSent,
+  });
+
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> challenge;
+  final bool isSent;
+
+  @override
+  State<ChallengesList> createState() => _ChallengesListState();
+}
+
+class _ChallengesListState extends State<ChallengesList> {
+  late final List<QueryDocumentSnapshot<Map<String, dynamic>>> requestList;
+  Map<int, Timer?> activeTimers = {}; // To track timers for each request
+
+  @override
+  void initState() {
+    requestList = widget.challenge;
+    // Cancel all active timers to avoid memory leaks
+    for (var timer in activeTimers.values) {
+      timer?.cancel();
+    }
+    super.initState();
+  }
+
+  void _loadData(
+    model.Notification request,
+    Map<String, dynamic> data,
+  ) {
+    if (widget.isSent) {
+      data['initialMessage'] = 'Your team ';
+      data['firstTeamName'] = request.challengeMatch!.challengerTeam.name;
+      data['middleMessage'] = ' has challenged team ';
+      data['secondTeamName'] = request.challengeMatch!.challengedTeam.name;
+      data['logoUrl'] = request.challengeMatch!.challengedTeam.logoUrl;
+    } else {
+      data['initialMessage'] = 'Team ';
+      data['firstTeamName'] = request.challengeMatch!.challengerTeam.name;
+      data['middleMessage'] = ' has challenged your team ';
+      data['secondTeamName'] = request.challengeMatch!.challengedTeam.name;
+      data['logoUrl'] = request.challengeMatch!.challengerTeam.logoUrl;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: requestList.length,
+      itemBuilder: (context, index) {
+        final challengeData = requestList[index];
+        final challenge = model.Notification.fromMap(challengeData.data());
+
+        final Map<String, dynamic> notificationData = {
+          'initialMessage': '',
+          'middleMessage': '',
+          'firstTeamName': '',
+          'secondTeamName': '',
+          'logoUrl': '',
+        };
+
+        _loadData(challenge, notificationData);
+        // Now data contains the updated values
+
+        return Container();
+      },
+    );
+  }
+
+  Future<void> _onCancelChallenge(int index, String notificationId) async {
+    Navigator.of(context).pop();
+    final result =
+        await NotificationServices.deleteNotification(notificationId, context);
+
+    if (result == 'success') {
+      setState(() {
+        requestList.removeAt(index);
+      });
+    }
+  }
+
+  void _toggleButton(String playerId, bool isAdding, WidgetRef ref) {
+    if (isAdding) {
+      ref.read(requestStatusProvider.notifier).addRequestInProgress(playerId);
+    } else {
+      ref.read(requestStatusProvider.notifier).addRequestSuccess(playerId);
+    }
+  }
+
+  Future<void> _acceptChallenge(
+    model.Notification request,
+    int index,
+    bool isPlayer,
+    WidgetRef ref,
+  ) async {
+    // Handle accept logic
+    _toggleButton(request.notificationId, true, ref);
+    try {
+      //! Acception challenge
+    } finally {
+      _toggleButton(request.notificationId, false, ref);
+    }
+  }
+
+  void _rejectChallenge(
+    BuildContext context,
+    int index,
+    QueryDocumentSnapshot<Map<String, dynamic>> requestData,
+  ) async {
+    // Cancel any existing timer for this index
+    activeTimers[index]?.cancel();
+
+    // Remove the request from the list and show the undo snack bar
+    setState(() {
+      requestList.removeAt(index);
+    });
+
+    bool isUndo = false;
+
+    showSnackBar('Request rejected', context, isUndo: true, onUndo: () {
+      isUndo = true;
+      setState(() {
+        requestList.insert(index, requestData);
+      });
+      activeTimers[index]?.cancel(); // Cancel the timer when undo is clicked
+    });
+
+    // Start a new timer for the current reject operation
+    activeTimers[index] = Timer(const Duration(seconds: 5), () {
+      if (!isUndo) {
+        // Perform the actual deletion
+        NotificationServices.deleteNotification(requestData.id, context);
+        activeTimers.remove(index); // Clean up the timer reference
+      }
+    });
+  }
+}
