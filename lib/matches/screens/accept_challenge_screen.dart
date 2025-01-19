78100c130320ad4a49307138f09388dd2ec24eaf
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-19 19:54:48 +0530

    Created some UI of AcceptMatchChallenge screen

---
+++ lib/matches/screens/accept_challenge_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:tracket/matches/widgets/team_column.dart';
+import 'package:tracket/utils/utility_classes/my_text_style.dart';
+import 'package:tracket/utils/utils.dart';
+import 'package:tracket/widgets/custom_widgets/my_card.dart';
+
+class AcceptChallengeScreen extends StatelessWidget {
+  const AcceptChallengeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Match Challenge  '),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //! Team Detail
+            MyCard(
+              child: Column(
+                children: [
+                  getTitleText('Teams', context),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    spacing: 8,
+                    children: [
+                      const TeamColumn(
+                        teamName: 'Challenged Team Name',
+                        teamLogo: '',
+                      ),
+                      Text(
+                        'v/s',
+                        style: MyTextStyle(context).boldBodyLarge,
+                      ),
+                      const TeamColumn(
+                        teamName: 'Challenger Team Name',
+                        teamLogo: '',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            //! Match detail
+            MyCard(
+              child: Column(
+                spacing: 20,
+                children: [
+                  getTitleText('Match Details', context),
+                  Column(
+                    spacing: 5,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      matchDetailRow('No of players', '11', context),
+                      matchDetailRow('Match Format', 'T20', context),
+                      matchDetailRow('Match type', 'challenge', context),
+                      matchDetailRow('Spectator', 'Allow', context),
+                      matchDetailRow('Date', '22 Jan 2025', context),
+                      matchDetailRow('Time', '5:00 pm', context),
+                      matchDetailRow('Venue', 'Wafa complex', context),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            MyCard(
+              child: Column(
+                children: [
+                  getTitleText('Squad', context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget matchDetailRow(String title, String value, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '$title : ',
+            style: MyTextStyle(context).boldBodyLarge,
+          ),
+          Text(
+            value,
+            style: MyTextStyle(context)
+                .boldBodyLarge
+                .copyWith(color: const Color.fromARGB(255, 50, 124, 53)),
+          )
+        ],
+      ),
+    );
+  }
+}
