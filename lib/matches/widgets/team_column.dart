a22dd7712e62e28382861b4212aa326504c4d1fb
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-30 19:52:23 +0530

    improve team column

--- lib/matches/widgets/team_column.dart
+++ lib/matches/widgets/team_column.dart
@@ -2,21 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:tracket/utils/utils.dart';
 
 class TeamColumn extends StatelessWidget {
-  const TeamColumn({super.key, required this.teamName, required this.teamLogo});
+  const TeamColumn({
+    super.key,
+    required this.teamName,
+    required this.teamLogo,
+    this.avatarRadius = 30.0,
+    this.spacing = 8.0,
+  });
 
   final String teamName;
   final String teamLogo;
+  final double avatarRadius;
+  final double spacing;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
-        spacing: 8,
+        spacing: spacing,
         children: [
-          getCircleAvatar(url: teamLogo, isTeam: true, radius: 30),
+          getCircleAvatar(
+            url: teamLogo,
+            isTeam: true,
+            radius: avatarRadius,
+          ),
           Text(
             teamName,
             textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
           ),
         ],
       ),
