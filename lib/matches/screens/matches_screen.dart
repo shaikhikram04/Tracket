07514707421b553cba134e5666ab83723d5e5f86
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-01-31 23:35:50 +0530

    started with match screen

--- lib/matches/screens/matches_screen.dart
+++ lib/matches/screens/matches_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tracket/matches/widgets/match_card.dart';
+import 'package:tracket/utils/colors.dart';
 
 class MatchesScreen extends StatelessWidget {
   const MatchesScreen({super.key});
@@ -7,12 +8,52 @@ class MatchesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: 4,
-        itemBuilder: (BuildContext context, int index) {
-          return const MatchCard();
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 184, 241, 186),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text('Completed'),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 107, 220, 111),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: blackColor)),
+                  child: Text('Live'),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 184, 241, 186),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text('Upcomming'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                itemCount: 4,
+                itemBuilder: (BuildContext context, int index) {
+                  return const MatchCard();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
