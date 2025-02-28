9db55e88bd468528ab94e99efdd81949c5b586d5
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-28 16:11:39 +0530

    created teamScore model for match model

---
+++ lib/matches/models/team_score.dart
@@ -0,0 +1,17 @@
+class TeamScore {
+  final runs;
+  final balls;
+  final wickets;
+
+  TeamScore({
+    required this.runs,
+    required this.balls,
+    required this.wickets,
+  });
+
+  // Computed properties
+  int get completedOvers => balls ~/ 6;
+  int get remainingBalls => balls % 6;
+
+  String get oversDisplay => '$completedOvers.${remainingBalls}';
+}
