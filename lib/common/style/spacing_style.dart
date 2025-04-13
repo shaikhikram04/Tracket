4b52cf15761e7db3a69765769282da48e0b4721a
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-04-13 23:11:40 +0530

    added spacing style

---
+++ lib/common/style/spacing_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tracket/utils/constants/sizes.dart';
+
+class TSpacingStyle {
+  static const paddingWithAppHeight = EdgeInsets.only(
+    top: TSizes.appBarHeight,
+    left: TSizes.defaultSpace,
+    right: TSizes.defaultSpace,
+    bottom: TSizes.defaultSpace,
+  );
+}
