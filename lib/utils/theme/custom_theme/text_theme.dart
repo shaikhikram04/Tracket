29b77ef62b5f09bc8de9399d5740898cf7eb193a
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-04-17 16:37:35 +0530

    added text theme for light & dark mode

---
+++ lib/utils/theme/custom_theme/text_theme.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme {
+  const TTextTheme._();
+
+  static TextTheme lightTextTheme = const TextTheme().copyWith(
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 18,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+    ),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.black,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.black.withValues(alpha: 0.5),
+      fontWeight: FontWeight.w500,
+    ),
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12,
+      color: Colors.black,
+      fontWeight: FontWeight.normal,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12,
+      color: Colors.black.withValues(alpha: 0.5),
+      fontWeight: FontWeight.normal,
+    ),
+  );
+
+  static TextTheme darkTextTheme = const TextTheme().copyWith(
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.white,
+      fontWeight: FontWeight.w400,
+    ),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.white,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14,
+      color: Colors.white.withValues(alpha: 0.5),
+      fontWeight: FontWeight.w500,
+    ),
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12,
+      color: Colors.white,
+      fontWeight: FontWeight.normal,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12,
+      color: Colors.white.withValues(alpha: 0.5),
+      fontWeight: FontWeight.normal,
+    ),
+  );
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tracket/utils/constants/colors.dart';
 
 class TracketTheme {
   const TracketTheme._();
 
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: const ColorScheme.light(
-      primary: Color(0xFF6200EE),
-      secondary: Color(0xFF03DAC6),
-      surface: Color(0xFFFFFFFF),
-      background: Color(0xFFF5F5F5),
-      error: Color(0xFFB00020),
-      onPrimary: Color(0xFFFFFFFF),
-      onSecondary: Color(0xFF000000),
-      onSurface: Color(0xFF000000),
-      onBackground: Color(0xFF000000),
-      onError: Color(0xFFFFFFFF),
-    ),
+    fontFamily: 'Rubik',
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: LightThemeColors.backgroundColor,
+    
   );
 }
