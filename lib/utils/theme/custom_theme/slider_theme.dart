e1b093d8590ff1837452cc317866e4cbcbda34f1
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-04-22 13:43:41 +0530

    made a slider theme

---
+++ lib/utils/theme/custom_theme/slider_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tracket/utils/constants/colors.dart';
+
+class TSliderTheme {
+  const TSliderTheme._();
+
+  static const SliderThemeData lightSliderTheme = const SliderThemeData(
+    thumbColor: primaryColor,
+    activeTrackColor: primaryColor,
+    valueIndicatorColor: primaryColor,
+    valueIndicatorTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+    ),
+    inactiveTrackColor: Colors.grey,
+  );
+
+  static const SliderThemeData darkSliderTheme = const SliderThemeData(
+    thumbColor: primaryLight,
+    activeTrackColor: primaryLight,
+    valueIndicatorColor: primaryLight,
+    valueIndicatorTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+    ),
+    inactiveTrackColor: Colors.grey,
+  );
+}
